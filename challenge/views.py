2d38b6a385911758622d8d45c182ce55a4258ebf
Akashraj <rajakash7001@gmail.con>
2021-05-16 17:22:33 +0530

    all commit

--- challenge/views.py
+++ challenge/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def jan(request):
     return HttpResponse("This works!")
+def feb(request):
+    return HttpResponse("hey we are now in february month")
